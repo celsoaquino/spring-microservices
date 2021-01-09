afa6c7c4ebeabdaebcafece868ba5cb1a0393789
celsoaquino <celso.aquino@ymail.com>
2021-01-09 18:09:50 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/celsoaquino/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/celsoaquino/hrworker/resources/WorkerResource.java
@@ -36,11 +36,11 @@ public class WorkerResource {
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
         //Test Hystrix
-       /* try {
+        try {
             Thread.sleep(3000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
 
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
